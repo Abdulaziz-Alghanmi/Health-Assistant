b5c2cfa788b81f160b40eb9b02564580235be79f
Abdulaziz Al-Ghanmi <62288783+Abdulaziz-Alghanmi@users.noreply.github.com>
2023-01-17 23:27:05 +0300

    Add files via upload

--- String reportStr(.js
+++ String reportStr(.js
@@ -1,7 +1,7 @@
 String reportStr( int normal, int high, int low, String type, String lastCondition) {
     
     if (low == 0 && high == 0) {
-      return 'All readings are good';
+      return 'Your readings have been stable and your health are good.\nEat healthy food and exercise every day for a healthy life.';
     } else if (normal == 0 && low == 0) {
       return 'All readings are High you should see the doctor';
     } else if (normal == 0 && high == 0) {
@@ -92,7 +92,37 @@ String reportStr( int normal, int high, int low, String type, String lastConditi
 
 } else if (low > normal || low > high){
     
-    if (lastCondition == "low") {
+    if (lastCondition == "high") {
+	
+        if(type == "Blood sugar"){
+        return 'Your ' + type +  ' level are high, if you have diabetes'+
+        ' the quickest way to reduce it is to take fast-acting insulin. Exercising is another fast, effective way to lower blood sugar. '
+        +' Change your eating diet way to a healthy diet, eating rich in natural nutrients and low in fat and calories. A key component of this diet is eating fruits,'+
+        ' vegetables and whole grains.\n'+
+        + 'Monitor your blood sugar level. Depending on your treatment plan, you can monitor and write down your blood sugar level several times a week or daily.'
+        +' Careful monitoring is the only way to ensure that your blood sugar level is maintained in the target range.'
+        + '\nIf you aren’t sure what to do, call your doctor to get instructions on administering a dose of insulin, '
+        +'and for advice about whether to go to the emergency room.';
+    
+    } else if (type == "Temperature"){
+        return 'Your ' + type + ' readings are high. Your body temperature start rising in the last couple of days. It could be caused by fever' + 
+        'you need to get lots of rest and drink plenty of fluids (water is best) to avoid dehydration' +
+        ' for your health you better visit your health care provider or call the emergency 998.\n';
+    
+    } else if (type == "Heart beat"){
+        return 'Your ' + type + ' readings are high. Your hearbeat rate are higher,'
++'An increased heart rate isn\'t always a problem. It\'s normal for your heart rate to increase during exercise or in response to stress.\n'
+        + ' If you have a history of heart disease and have palpitations that occur frequently or worsen, talk to your health care provider.'+
+        ' You may need heart-monitoring tests to see if the palpitations are caused by a more serious heart problem. \n';
+    
+    }else if (type == "pressure"){
+        return 'Your Blood ' + type + ' readings are high. Adopting healthy lifestyle habits such as not smoking, exercising and eating a healthy diet can help prevent and treat high blood pressure. Decreasing salt in your diet, '+
+        'losing weight if necessary, stopping smoking, cutting down on alcohol use, and regular exercise. because high blood pressure is a long-lasting medical condition that often has little or no symptoms, remembering to take your medications can be a challenge.'+
+        ' Combination medicines, long-acting or once-a-day medications, may be used to decrease the burden of taking numerous medications and help ensure medications regularly. Once started, the medication should be used until your doctor tells you to stop. ';
+    }
+
+
+      }else if (lastCondition == "low") {
 	
         if(type == "Blood sugar"){
         return 'Your last ' + type +  ' level are low, Treat low blood sugar right away. You can quickly raise your blood sugar level by eating a source of simple sugar, such as glucose tablets, candy or fruit juice.'
@@ -204,5 +234,6 @@ String reportStr( int normal, int high, int low, String type, String lastConditi
         }else if (lastCondition == "normal") {
     return 'Your readings have been stable and your health are good.\nEat healthy food and exercise every day for a healthy life.'           
         }
+}
 }
 }
